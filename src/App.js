87675777e8158ca23ebe1de8785fb065717317bb
vspeeeeee <www.vishnusureshperumbavoor@gmail.com>
2022-05-08 10:59:29 +0530

    deleted netflix-clone folder. added documentaries, comedy, romance, horror moveis to the list. added hovering property to small films

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
 import Rowpost from './Components/Rowpost/Rowpost';
-import {action, originals} from './urls'
+import {action, documentaries, originals, romance, comedy, horror} from './urls'
 function App() { 
   return(
     <div className="App">
       <Navbar/>
       <Banner/>
       <Rowpost title="netflix originals" url={originals} />
-      <Rowpost title="action movies" tovino url={action} />
+      <Rowpost title="action movies" url={action} isSmall />
+      <Rowpost title="Documentaries" url={documentaries} isSmall />
+      <Rowpost title="Comedy" url={comedy} isSmall />
+      <Rowpost title="Horror" url={horror} isSmall />
+      <Rowpost title="Romance" url={romance} isSmall />
     </div>
   )  
 }
--- src/Components/Rowpost/Rowpost.jsx
+++ src/Components/Rowpost/Rowpost.jsx
@@ -10,38 +10,39 @@ function Rowpost(props) {
         axios.get(props.url).then((response)=>{
             console.log(response.data);
             setMovies(response.data.results)
+        }).catch(err=>{
+            // alert('network error')
         })
     }, [])
-
     const opts = {
         height: '390',
         width: '100%',
         playerVars: {
-          // https://developers.google.com/youtube/player_parameters
           autoplay: 0,
         },
-      };
-
-      const handleMovie = (id)=>{
+    };
+    const handleMovie = (id)=>{
           console.log(id);
           axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-              if(response.data.results.length!=0){
+              if(response.data.results.length!==0){
                   setUrlId(response.data.results[0])
               }else{
                   console.log("Array empty");
               }
           })
       }
-
     return (
         <div className="row">
             <h1>{props.title}</h1>
             <div className="posters">
                 {movies.map((obj)=>
-                    <img onClick={()=>handleMovie(obj.id)} className={props.tovino ? 'smallPoster' : "poster"} src={`${imageUrl+obj.backdrop_path}`} alt="" />                
+                    <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} 
+                    src={`${imageUrl+obj.backdrop_path}`} alt="" />                
                 )}
             </div>
             { urlId && <Youtube videoId={urlId.key} opts={opts}  /> }
+
+            
         </div>
     )
 }
